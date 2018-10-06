2d720ef8c2b98a47baeb2491cf25c9e78573bf08
Joe LaPenna <jlapenna@gmail.com>
2018-10-06 14:47:33 -0700

    Add auth decorator

--- gae/backend/auth_util.py
+++ gae/backend/auth_util.py
@@ -1,3 +1,7 @@
+# Helper library for authentication.
+
+import functools
+
 # Use the App Engine Requests adapter. This makes sure that Requests uses
 # URLFetch. This needs to be ordered the way it is, for some reason.
 # https://github.com/firebase/firebase-admin-python/issues/185
@@ -6,17 +10,34 @@ import requests_toolbelt.adapters.appengine
 requests_toolbelt.adapters.appengine.monkeypatch()
 HTTP_REQUEST = google.auth.transport.requests.Request()
 
+import flask
+
 import google.oauth2.id_token
 from firebase_admin import auth
 
 
-def verify_claims(request):
+def auth_required(func):
+    @functools.wraps(func)
+    def wrapper():
+        creds = verify(flask.request)
+        return func()
+    return wrapper
+
+
+def verify(request):
+    if request.method == 'POST':
+        return verify_claims_from_header(request)
+    elif request.method == 'GET':
+        return verify_claims_from_cookie(request)
+
+
+def verify_claims_from_header(request):
     """Return valid claims or throw an AuthError."""
     id_token = request.headers['Authorization'].split(' ').pop()
     claims = google.oauth2.id_token.verify_firebase_token(
         id_token, HTTP_REQUEST)
     if not claims:
-        raise auth.AuthError('Unable to find valid token')
+        raise auth.AuthError(401, 'Unable to find valid token')
     return claims
 
 
@@ -29,5 +50,5 @@ def verify_claims_from_cookie(request):
     try:
         return auth.verify_session_cookie( session_cookie, check_revoked=True)
     except ValueError, e:
-        raise auth.AuthError('Unable to validate cookie')
+        raise auth.AuthError(401, 'Unable to validate cookie')
 
