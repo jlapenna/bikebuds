c3a478cfbbe8700633c9f0bcb8041cde869d113c
Joe LaPenna <jlapenna@gmail.com>
2019-04-21 16:53:54 -0700

    Add single flutter test, replacing broken test.

---
+++ flutter/test/profile_card_test.dart
@@ -0,0 +1,40 @@
+// Copyright 2019 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:bikebuds/profile_card.dart';
+import 'package:bikebuds_api/bikebuds/v1.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'bikebuds_api_testutil.dart';
+import 'firebase_testutil.dart';
+
+void main() {
+  testWidgets('profile card smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+
+    FirebaseUserMock mockFirebaseUser = newFirebaseUserMock();
+    MainProfileResponse fakeProfileResponse = newProfileResponse();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ProfileCard(mockFirebaseUser, fakeProfileResponse),
+      ),
+    );
+
+    expect(find.text('Test Name'), findsOneWidget);
+
+    expect(find.text('San Francisco'), findsOneWidget);
+  });
+}
