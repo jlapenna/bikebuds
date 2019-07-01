e0db40ae146204b7aecc8bd564f5d59c5f5739ef
Joe LaPenna <jlapenna@gmail.com>
2019-06-30 17:53:29 -0700

    GAE: Clean up fcm_util for better debugging.

--- gae/shared/fcm_util.py
+++ gae/shared/fcm_util.py
@@ -20,6 +20,7 @@ from google.cloud.datastore.entity import Entity
 from firebase_admin import messaging
 
 from shared import ds_util
+from shared.datastore.fcm import FcmMessage, FcmSendEvent
 
 
 def active_clients(user_key):
@@ -45,9 +46,10 @@ def send(user_key, clients, notif_fn, *args, **kwargs):
             a ClientMessage will be passed as a 'client' kwarg.
     """
     with ds_util.client.transaction():
-        fcm_message = Entity(ds_util.client.key('FcmSendEvent', parent=user_key))
-        fcm_message['date'] = datetime.datetime.utcnow()
-        fcm_message['messages'] = []
+        fcm_send_event = FcmSendEvent.to_entity({
+            'date': datetime.datetime.utcnow(),
+            'messages': [],
+            }, parent=user_key)
         logging.debug('Sending notification to %s clients', len(clients))
         for client_state in clients:
             message = notif_fn(*args, client=client_state, **kwargs)
@@ -55,30 +57,28 @@ def send(user_key, clients, notif_fn, *args, **kwargs):
                 response = messaging.send(message)
                 logging.debug('fcm_util.send: Success: %s, %s, %s', user_key, message,
                         response)
-                _add_delivery(fcm_message, client_state, message, response)
+                _add_delivery(fcm_send_event, client_state, message, response)
             except Exception as e:
                 logging.exception('fcm_util.send: Failure: %s', user_key)
-                _add_delivery(fcm_message, client_state, message, e)
-        ds_util.client.put(fcm_message)
+                _add_delivery(fcm_send_event, client_state, message, e)
+        ds_util.client.put(fcm_send_event)
 
 
-def _add_delivery(fcm_message, client_store, message, response):
+def _add_delivery(fcm_send_event, client_store, message, response):
     success = not isinstance(response, Exception)
     if message.data is not None:
-        e = Entity(ds_util.client.key('Data'))
-        e.update(dict(
-            response=str(response),
-            client_store=client_store.key,
-            success=success))
-        fcm_message['messages'].append(e)
+        e = FcmMessage.to_entity({
+            'response': str(response),
+            'client_store': client_store.key,
+            'success': success})
+        fcm_send_event['messages'].append(e)
     elif message.notification is not None:
-        e = Entity(ds_util.client.key('Notification'))
-        e.update(dict(
-            response=str(response),
-            client_store=client_store.key,
-            success=success,
-            title=message.notification.title,
-            body=message.notification.body))
-        fcm_message['messages'].append(e)
+        e = FcmMessage.to_entity({
+            'response': str(response),
+            'client_store': client_store.key,
+            'success': success,
+            'title': message.notification.title,
+            'body': message.notification.body})
+        fcm_send_event['messages'].append(e)
     else:
         logging.error('Cannot add delivery of unknown message type')
