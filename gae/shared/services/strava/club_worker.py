085baeb6d1f21168d64377d4b2aaba5c0fe4e8e8
Joe LaPenna <jlapenna@gmail.com>
2021-01-10 14:14:02 -0800

    GAE: fix typo, activity_query -> query

--- gae/shared/services/strava/club_worker.py
+++ gae/shared/services/strava/club_worker.py
@@ -45,9 +45,7 @@ class ClubWorker(object):
 
         with ds_util.client.transaction():
             club = Club.get(self.club_id, parent=self.service.key)
-            activity_query = ds_util.client.activity_query(
-                kind='Activity', ancestor=club.key
-            )
+            activity_query = ds_util.client.query(kind='Activity', ancestor=club.key)
             activity_query.keys_only()
             ds_util.client.delete_multi(
                 activity.key for activity in activity_query.fetch()
